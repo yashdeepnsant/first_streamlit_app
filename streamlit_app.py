68ccab10ec5864d91be79ba16303e8ee5848d64b
yashdeepnsant <107410363+yashdeepnsant@users.noreply.github.com>
2022-06-13 21:17:11 +0530

    Update streamlit_app.py
    Importing pandas, reading csv from S3 and displaying using streamlit data frames

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,15 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard Boiled free-range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
