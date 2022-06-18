dc49429db80ca537daecd8176ed0c6d7c1aa3cd1
yashdeepnsant <107410363+yashdeepnsant@users.noreply.github.com>
2022-06-18 17:50:54 +0530

    Update streamlit_app.py
    Added function to insert new record in snowflake DB

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,12 +55,15 @@ if streamlit.button('Get Fruit Load list'):
 #my_data_row = my_cur.fetchone()
 #streamlit.text("Hello from Snowflake:")
 
+#streamlit.stop()
 
-
-
-
-streamlit.stop()
-
-add_fruit = streamlit.text_input('What Fruit you would like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_fruit)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+def insert_row_snowflake (new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        retrun "Thanks for adding ", new_fruit
+        
+add_my_fruit = streamlit.text_input('What Fruit you would like to add?')
+if streamlit.button('Add a Fruit to the list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake (add_my_fruit)
+    streamlit.text(back_from_function)
