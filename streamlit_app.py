b8e4ed41fb501c6beb33145c30c82306d372c8f4
yashdeepnsant <107410363+yashdeepnsant@users.noreply.github.com>
2022-06-18 17:55:57 +0530

    Update streamlit_app.py
    parameterized insert statement based on user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ if streamlit.button('Get Fruit Load list'):
 
 def insert_row_snowflake (new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        my_cur.execute("insert into fruit_load_list values ('"+ new_fruit +"')")
         return "Thanks for adding "+ new_fruit
         
 add_my_fruit = streamlit.text_input('What Fruit you would like to add?')
