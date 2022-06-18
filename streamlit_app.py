94a2e47a5b355656e67359413b68c7bb59a7b454
yashdeepnsant <107410363+yashdeepnsant@users.noreply.github.com>
2022-06-18 17:53:01 +0530

    Update streamlit_app.py
    again silly mistake in return spelling

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ if streamlit.button('Get Fruit Load list'):
 def insert_row_snowflake (new_fruit):
     with my_cnx.cursor() as my_cur:
         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-        retrun "Thanks for adding "+ new_fruit
+        return "Thanks for adding "+ new_fruit
         
 add_my_fruit = streamlit.text_input('What Fruit you would like to add?')
 if streamlit.button('Add a Fruit to the list'):
