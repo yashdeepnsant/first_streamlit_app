e333676826c1df873d0c097d38ccd14945e427ee
yashdeepnsant <107410363+yashdeepnsant@users.noreply.github.com>
2022-06-18 17:44:22 +0530

    Update streamlit_app.py
    correct spelling of cursor

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ except URLError as e:
 
 streamlit.header("The Fruit List contains:")
 def get_fruit_load_list():
-    with  my_cnx.cusror() as my_cur:
+    with  my_cnx.cursor() as my_cur:
            my_cur.execute("select * from fruit_load_list")
            return my_cur.fetchall()
 if streamlit.button('Get Fruit Load list'):
